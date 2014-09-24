06e2ca4ca04d95ed2c1eb4dc67d4529c6cea9770
Pavel Serdiukov <serdukov.p.a@gmail.com>
2014-09-24 15:52:49 +0700

    Add bower.js and wrap code

---
+++ lodash-angular-wrapper.js
@@ -0,0 +1,16 @@
+'use strict';
+angular.module('angularLodashWrapper',[])
+.run(function(){
+	if (!window || !window._) {
+            throw new Error( "Js library 'Lo-Dash' is not available! You either misspelled " +
+                "the lib name or forgot to load it. Please check http://lodash.com/" );
+        }
+})
+.factory('_', function() {
+        if (window && window._) {
+            return window._;
+        } else {
+            throw new Error( "Js library 'Lo-Dash' is not available! You either misspelled " +
+                "the lib name or forgot to load it. Please check  http://lodash.com/" );
+        }
+});
